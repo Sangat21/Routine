f8dee4490ae9ecff8b80511d0723ec697f58a6a2
Sunny Shah <sangatshah1@gmail.com>
2020-09-02 17:06:24 -0700

    Fixing issues with resetting values at midnight
    NOT FIXED YET!!

--- Routine/myScreens/RoutineList.screen.js
+++ Routine/myScreens/RoutineList.screen.js
@@ -6,7 +6,9 @@ import {
   FlatList
 } from 'react-native';
 import {useAsyncStorage} from '@react-native-community/async-storage';
-import {RoutineBtn} from '../myComponents/RoutineBtn.component.js'
+import AsyncStorage from '@react-native-community/async-storage';
+import {RoutineBtn} from '../myComponents/RoutineBtn.component.js';
+import moment from 'moment';
 
 export const RoutineList = (props) => {
 
@@ -29,8 +31,50 @@ export const RoutineList = (props) => {
         await setItem(stringVal);
     }
 
+    // function to store date
+    const storeDate = async (value) => {
+      try {
+        await AsyncStorage.setItem('@date_storage_Key', value)
+        } catch {
+
+        }
+    }
+
+    // function to detect whether day has changed
+    const checkDay = async () => {
+        var today = moment(new Date()).format('MM-DD-yyyy');
+        var storageDate = await AsyncStorage.getItem('@date_storage_Key');
+        // console.log("today: ", today);
+        // console.log("storage: ", storageDate);
+        if(storageDate == null || storageDate != today){
+            storeDate(today);
+            let newList = allRoutines;
+            console.log("NewList: ", newList);
+            newList.forEach((item) => {
+                item.timeLeft = item.time + ":00";
+            });
+            console.log("newer List: ", newList);
+            writeItemToStorage(newList);
+        }
+        //console.log(today);
+    }
+
+    //console.log(moment("24:00:00", "hh:mm:ss").diff(moment()));
+    setTimeout(() => {
+        let newList = allRoutines;
+        console.log("NewList: ", newList);
+        newList.forEach((item) => {
+            item.timeLeft = item.time + ":00";
+        });
+        console.log("newer List: ", newList);
+        writeItemToStorage(newList);
+    }, moment("24:00:00", "hh:mm:ss").diff(moment()) );
+
+
     useEffect(() => {
         getItemFromStorage();
+        //storeDate(moment("12-25-1995", "MM-DD-YYYY").format('MM-DD-yyyy')); // testing date change
+        //checkDay();
     })
 
     // if allRoutines is null, set to []
